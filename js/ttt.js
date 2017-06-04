e7c3153408092419f53e236bda9ebb276b9e0276
a-rutnam <a.rutnam@gmail.com>
2017-06-04 16:35:00 +1000

    score started

--- js/ttt.js
+++ js/ttt.js
@@ -1,45 +1,68 @@
 var game = [0,0,0,0,0,0,0,0,0];
 var gameWon = false;
-var $img = $('<img>').attr({ src: ''
-});
-
-var player = 1; //change this depending on what random_boolean chooses
+var $img = $('<img>').attr({ src: ''});
+//maybe a separate player 1 and 2 $img
+var player = 1;
 var playerNames = [];
 
 $(document).ready(function () {
-  // player names
-  $( "#nameEnterButton" ).click(function() {
-    nameInput = $('#nameInput').val()
-    playerNames.push(nameInput)
-    $('#nameInput').val("");
-    $('input').attr('placeholder',"Enter Player 2's name");
-    playerNames;
-    if (playerNames.length === 2) { alert("Now click 'Who plays first' below.")
-      $('input').attr('placeholder',"Click 'Who Plays' below.");
-    }
-    return;
-  });
 
-  // who plays first:
-  $('#demo').on('click', function(event) {
-    var player1 = playerNames[0]
-    var player2 = playerNames[1]
 
-    var random_boolean = Math.random() >= 0.5; {
-      if (random_boolean) {
-      alert (player1 + " decides who plays first. Anchors aweigh - Start Playing!");
-      } else {
-      alert (player2 + " decides who plays first. Anchors aweigh - Start Playing!");
-      } //else
-    }//rando bools.
-  });//who plays first button
 
-  // reset
-  $( "#reset" ).click(function() {
-    $('.box img').remove();
-    game = [0,0,0,0,0,0,0,0,0]
-    player = 1;
-  });//reset button
+
+    // player names
+    $( "#nameEnterButton" ).click(function() {
+      nameInput = $('#nameInput').val() //string typed to input box
+
+      playerNames.push(nameInput)//name 1st or 2nd is pushed to array
+
+      $('#nameInput').val("");//gets rid of input after click
+
+      $('input').attr('placeholder',"Enter Player 2's name");//adds p2 prompt placeholder
+
+      playerNames;//why is this here
+
+      if (playerNames.length === 1)
+        {$( ".vs" ).html( "<h1>"+playerNames[0]+" Vs. "+"</h2>" );//first name entered appears beneath board
+      }
+
+      if (playerNames.length === 2) {
+        alert("Now click 'Who plays first' below.")
+
+        $('input').attr('placeholder',"Click 'Who Plays' below.");//who plays prompt after 2 names entered
+
+        $( ".vs" ).html( "<h1>"+playerNames[0]+" Vs. "+playerNames[1]+"</h2>" );
+      }
+
+      if (playerNames.length === 3) {
+          // playerNames[2] = null;// not Necessary for rando below to work so need i worry?
+          alert("Only two players, you scoundrel! This isn't 'Naughts and Cross and Another Thing!'");
+          return;
+        }
+      return;
+    });//name button clicked
+
+
+
+
+    // who plays first:
+    $('#whoFirst').on('click', function(event) {
+      var firstNameEntered = playerNames[0]
+      var secondNameEntered = playerNames[1]
+
+      var random_boolean = Math.random() >= 0.5; {
+        if (random_boolean) {
+          alert (firstNameEntered + " decides who plays first. Anchors aweigh - Start Playing!");
+        } else {
+          alert (secondNameEntered + " decides who plays first. Anchors aweigh - Start Playing!");
+        } //else. make shorter
+      }//rando bools.
+    });//who plays first button. Ideally now picked player picks their token and goes
+
+
+// Jim and Jerry.
+// Jerry is picked by rando to go first. Therefore player 1 is Jerry.
+//Jerry clicks on a token image and the src of it becomes what is appended when he click a square
 
   console.log( "Let's Play!" );
 
@@ -52,12 +75,12 @@ $(document).ready(function () {
         (game[2] === player && game[5] === player && game[8] === player) || //col3
         (game[0] === player && game[4] === player && game[8] === player) || //diag1
         (game[2] === player && game[4] === player && game[6] === player)){ //diag2
-          // console.log(player + ' wins');
           return true;
         }//if true do this
     return false; //what does this do again?
   };//checkwins
 
+//IF OCCUPIED
   $('.box').on('click', function(event) {
     var id = +event.currentTarget.id //the plus is a quicky way to parseint
     if (game[id] !== 0){
@@ -68,15 +91,15 @@ $(document).ready(function () {
 
   console.log('player '+player+ ' clicked on Square '+event.currentTarget.id);
 
-  //if player 1:
+  //if player 1: this makes player 1 always x, change that
   if (player === 1){
     game[id]='x';
+
     gameWon = checkWin('x');
     if (gameWon){
       alert('X marks the spot!... (X wins)');
       console.log('X wins!');
     }//if x won
-
     var $img = $('<img>').attr({
       id: 'img' + id,
       src: 'img/x.gif'
@@ -84,6 +107,7 @@ $(document).ready(function () {
     $(this).append( $img );
     player = 2;
     return;
+
   } else {
     game[id]='o';
     gameWon = checkWin('o');
@@ -101,95 +125,25 @@ $(document).ready(function () {
   }//else... p2
 
   // check for draw
-  if ( !gameWon && !game.includes(0) ){
-    alert("It's a stalemate. To the poopdeck with all of you!")
-    console.log('stalemate'); //this isn't running
-
-  }//check tie
+    if ( !gameWon && !game.includes(0) ){
+      alert("It's a stalemate. To the poopdeck with all of you!")
+      console.log('stalemate'); //this isn't running
+    }//check tie
 
   }); //box
 
+
+    // RESET
+    $( "#reset" ).click(function() {
+      $('.box img').remove();
+      game = [0,0,0,0,0,0,0,0,0]
+      player = 1;
+
+
+    $("#b").animate({left: "+=500"}, 2000);
+    
+
+
+    });//reset button
+
 })//doc ready;
-
-
-
-// Project #0: The Game
-//
-// Overview
-//
-// Let's start out with something fun - a game!
-//
-// We'll be making Tic Tac Toe, a game that takes seconds to learn but minutes to master! Everyone will get a chance to be creative, and work through some really tough programming challenges to get your feet wet in the world of web development.
-//
-// "hand me your phone," load up the game, and play a quick round!
-// You will be working individually for this project, but we'll be guiding you along the process and helping as you go. Show us what you've got!
-//
-// What You've Learned
-//
-// By the time you submit this project, you will have covered new ground in many of the big themes of the course:
-//
-// Command Line: Practice interacting with the computer and navigating the filesystem from the command line.
-// Source Control: Manage and interact with a git repository to store changes to code.
-// Programming Fundamentals: Work with objects, constructors, events, while learning how to strategically solve problems and resolve errors.
-// Web Fundamentals: Learn how communication happens over the internet, and how to structure, style, and animate documents within a browser. Also learn how to respond to actions your users take and the data they input into the browser.
-// Browser Applications: Dive into CSS, Sass, and how to use libraries and frameworks to get lots of style for free.
-// Deployment: Host a static web site in a managed hosting environment.
-// Products and Teams: Document your code and your code repository so others understand what you've built.
-// Big Goals
-//
-// Build a web application from scratch, without a starter codebase
-// Use your programming skills to map out the game logic for a simple game like Tic Tac Toe
-// Separate HTML, CSS, and JavaScript files in your application
-// Build an application to a spec that someone else gives you
-// Build a dynamic game that allows two players to compete
-// Craft a readme.md file that explains your app to the world
-// Technical Requirements
-//
-// Your app must:
-//
-// Render a game board in the browser
-// Switch turns between X and O (or whichever markers you select)
-// Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
-// Include separate HTML / CSS / JavaScript files
-// Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
-// Use Javascript for DOM manipulation
-// Deploy your game online, where the rest of the world can access it
-// Use semantic markup for HTML and CSS (adhere to best practices)
-// Bonus
-//
-// These are for extra credit! Don't focus on these until you've hit the core requirements.
-//
-// Keep track of multiple game rounds with a win counter
-// Allow players to customize their tokens (X, O, name, picture, etc)
-// Get inventive with your styling, e.g. use hover effects or animations to spiff things up
-// Use LocalStorage to persist data locally to allow games to continue after page refresh or loss of internet connectivity
-// Support custom board sizes: default is 3x3 but you could allow users to choose a larger board
-// Support networked multiplayer: https://www.firebase.com/ has a nice quickstart guide
-// TRICKIEST: Create an AI opponent: teach Javascript to play an unbeatable game against you
-// Necessary Deliverables
-//
-// A working game, built by you, hosted somewhere on the internet
-// A link to your hosted working game in the URL section of your Github repo
-// A git repository hosted on Github, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-// A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
-// Suggested Ways to Get Started
-//
-// Break the project down into different components (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
-// Use your Development Tools (console.log, inspector, alert statements, etc) to debug and solve problems
-// Work through the lessons in class, ask questions and come to office hours when you need to. Think about adding relevant code to your Tic Tac Toe game each night, instead of, you know... procrastinating.
-// Commit early, commit often. Don’t be afraid to break something because you can always go back in time to a previous version.
-// Check out Tutorial and Documentation resources (jQuery tutorial) at home to better understand what you’ll be getting into.
-// Don’t be afraid to write code that you know you will have to remove later. Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
-// Useful Resources
-//
-// MDN Javascript Docs (a great reference for all things Vanilla Javascript)
-// jQuery Docs
-// Github Pages (for hosting your game)
-// If You Finish Early
-//
-// We invite you to work on any or all of the following:
-//
-// A more advanced game (Memory? Battleship? Connect Four?)
-// Your Github portfolio site
-// Any other front-end project that interests you
-// Use Firebase for something
--- readme.md
+++ readme.md
@@ -3,17 +3,22 @@
 
 here is [my live site](https://a-rutnam.github.io/tictactoe/)
 
-Game instructions
+###Libraries used
+- JQuery
+
+
+###Game instructions
 1. Begin by writing player 1 and then player 2's names into the input box.
 2. By clicking the 'who will play first' button, the website will choose at random who will use the symbol 'x' and therefore play first.
 3. Play turn by turn and the game will detect and announce whether there has been a win or a draw.
 
 
 
-If I had more time
+###If I had more time
 - Prevent squares from being filled after win.
 - Have on option for existing players to 'play again' (and run random player chooser without having to renter names)
-- Develop multiple visual themes e.g. space, crafty, Medieval etc. Plus a way for players to upload an image and choose their own token.
+- Develop multiple visual themes e.g. space, crafty, Monopoly and other games better than T3, etc. Plus a way for players to upload an image and choose their own token.
+- Fix weird-ass dual colored border
 - Checking for the same name entered twice
 
 
